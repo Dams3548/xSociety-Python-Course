8b5dfb36e31c81b001cbc54ce0cfb1e8ac1b1fbc
Dams3548 <146871356+Dams3548@users.noreply.github.com>
2023-12-07 17:33:57 +0000

    Add files via upload
    Added the entire course to the repository

---
+++ Python/Week 2/Cheese.py
@@ -0,0 +1,8 @@
+import time
+Cheese = str(input("Do you like cheese? "))
+if Cheese == "yes":
+    print(f"You like Cheese")
+else:  
+    print("You don't like cheese")
+    time.sleep(5) 
+    print("There is a body behind the freezer in my kitchen")
